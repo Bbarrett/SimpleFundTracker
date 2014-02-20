e0722e7bda01f46b1f304a11b11c361f466176cc
Brandon Barrett <bbarrett@1105media.com>
2014-02-19 21:43:26 -0500

    Create new class to clean document and delete duplicate Strings in file.

---
+++ SimpleFundTracker/src/simplefundtracker/DocumentCleaner.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package simplefundtracker;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author brandonbarrett
+ */
+public class DocumentCleaner extends Recorder {
+    
+    /**
+     *
+     * @throws java.io.IOException
+     */
+    private BufferedReader myBr;
+    private BufferedWriter myBw;
+    
+    public DocumentCleaner() throws IOException{
+        
+    }
+
+}
