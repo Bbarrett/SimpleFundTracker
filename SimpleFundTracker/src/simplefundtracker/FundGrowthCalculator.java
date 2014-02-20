595ceb829ac9d45bd16228faf0a5f22b33b4a268
Brandon Barrett <bbarrett@1105media.com>
2014-02-19 19:57:27 -0500

    push growth calculator class

---
+++ SimpleFundTracker/src/simplefundtracker/FundGrowthCalculator.java
@@ -0,0 +1,21 @@
+package simplefundtracker;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author brandonbarrett
+ */
+public class FundGrowthCalculator {
+    private int months;
+    private final int percentGrowth;
+    
+public FundGrowthCalculator(){
+    this.months = 0;
+    this.percentGrowth = 0;
+}
+}
