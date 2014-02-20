2efe803deb85758a380fef53d294a93c9c286ed8
Brandon Barrett <bbarrett@1105media.com>
2014-02-19 22:08:31 -0500

    Start writing documentCleaner method to kill duplicate dates in file.

--- SimpleFundTracker/src/simplefundtracker/DocumentCleaner.java
+++ SimpleFundTracker/src/simplefundtracker/DocumentCleaner.java
@@ -8,7 +8,12 @@ package simplefundtracker;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Date;
 
 /**
  *
@@ -20,11 +25,29 @@ public class DocumentCleaner extends Recorder {
      *
      * @throws java.io.IOException
      */
-    private BufferedReader myBr;
-    private BufferedWriter myBw;
+  
+ 
+  
+  private Recorder myRecorder = new Recorder();
+    private File fileToCheck;
+    private Date dateToCheck;
     
     public DocumentCleaner() throws IOException{
-        
+    //Initialize these bad boys...
+    fileToCheck = myRecorder.getFundFile();
+    dateToCheck = myRecorder.getDate();
+   
+       
     }
+  public void cleanDocument(File fileToCheck, Date dateToCheck) throws FileNotFoundException, IOException{
+    BufferedReader myBr = new BufferedReader(new FileReader(fileToCheck));
+    BufferedWriter myBw = new BufferedWriter(new FileWriter(fileToCheck, true));
+    String fileLine;
+    
+    while ((fileLine = myBr.readLine()) != null) {
+            myBr.readLine();
+
+            
+    
 
 }
