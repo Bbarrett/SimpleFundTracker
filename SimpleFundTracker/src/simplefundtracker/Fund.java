2f5dabb6f61dff19f6929e2d26fdf17d21382639
Brandon Barrett <bbarrett@1105media.com>
2014-02-19 19:33:36 -0500

    Commit class, clean up commenting and formatting.

---
+++ SimpleFundTracker/src/simplefundtracker/Fund.java
@@ -0,0 +1,29 @@
+
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+ */
+package simplefundtracker;
+
+/**
+ *
+ * @author brandonbarrett
+ */
+public class Fund {
+    
+    private final String FUNDNAME;
+    
+    public Fund(){
+    
+    FUNDNAME = "Fidelity Freedom 2050";
+        
+    }
+   
+    public String getFundName() {
+        return FUNDNAME;
+    }
+}
+
+
+
