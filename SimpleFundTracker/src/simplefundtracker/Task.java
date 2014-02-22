390ad4e453598c5826b83779663969e62749b73e
Brandon Barrett <bbarrett@1105media.com>
2014-02-22 12:44:35 -0500

    Update to include URLScanner ref.

--- SimpleFundTracker/src/simplefundtracker/Task.java
+++ SimpleFundTracker/src/simplefundtracker/Task.java
@@ -1,9 +1,7 @@
-
 package simplefundtracker;
 
 //~--- JDK imports ------------------------------------------------------------
 
-import java.io.File;
 import java.io.IOException;
 
 import java.util.TimerTask;
@@ -15,11 +13,15 @@ import java.util.logging.Logger;
  * @author brandonbarrett
  */
 public final class Task extends TimerTask {
-    private Recorder  r;
-    private Connector c;
-    private String    todaysFund;
+    private Recorder         r;
+    private Connector        c;
+    private String           todaysFund;
+    private final URLScanner URLSC;
 
-    public Task() throws IOException {}
+    public Task() throws IOException {
+        URLSC = new URLScanner();
+        URLSC.setURL();
+    }
 
     // Make it simple, create a task that can be set to run each day at noon.
     @Override
@@ -27,7 +29,10 @@ public final class Task extends TimerTask {
         try {
             r = new Recorder();
             c = new Connector();
-            c.findFund();
+
+            String fundURL = URLSC.getURL();
+
+            c.findFund(fundURL);
             r.createHeader();
             todaysFund = c.getfundElement();
             r.createFund(todaysFund);
