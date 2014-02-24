39ad35ec986344cee73290151f9028eab5a36a0e
Brandon Barrett <bbarrett@1105media.com>
2014-02-24 18:34:18 -0500

    Update formatting

--- SimpleFundTracker/src/simplefundtracker/FundGrowthCalculator.java
+++ SimpleFundTracker/src/simplefundtracker/FundGrowthCalculator.java
@@ -9,30 +9,31 @@ public class FundGrowthCalculator {
     private final int PERCENTGROWTH;
     private Double yearGrowthRate;
     private Double decadeGrowthRate;
-    
-public FundGrowthCalculator(){
-    this.months = 0;
-    this.PERCENTGROWTH = 0;
-}
-public Double calculateYearGrowth(Double averageChange) {
-    
-    yearGrowthRate =  averageChange * 52.0;
-    return yearGrowthRate;
-    
+
+    public FundGrowthCalculator() {
+        this.months = 0;
+        this.PERCENTGROWTH = 0;
+    }
+
+    public Double calculateYearGrowth(Double averageChange) {
+        yearGrowthRate = averageChange * 52.0;
+
+        return yearGrowthRate;
+    }
+
+    public Double calculateDecadeGrowth(Double yearGrowthRate) {
+        decadeGrowthRate = yearGrowthRate * 10.0;
+
+        return decadeGrowthRate;
+    }
+
+    public Double getYearGrowth() {
+        return yearGrowthRate;
+    }
+
+    public Double getDecadeGrowth() {
+        return decadeGrowthRate;
+    }
 }
 
-public Double calculateDecadeGrowth(Double yearGrowthRate) {
-    
-    decadeGrowthRate = yearGrowthRate * 10.0;
-    return decadeGrowthRate;
-}
 
-public Double getYearGrowth() {
-    return yearGrowthRate;
-}
-
-public Double getDecadeGrowth() {
-    return decadeGrowthRate;
-}
-
-}
