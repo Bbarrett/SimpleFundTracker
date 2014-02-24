d448bb32d1563f308336291683ee2f52c8c0b967
Brandon Barrett <bbarrett@1105media.com>
2014-02-24 18:32:42 -0500

    Update formatting comment

--- SimpleFundTracker/src/simplefundtracker/FundGrowthCalculator.java
+++ SimpleFundTracker/src/simplefundtracker/FundGrowthCalculator.java
@@ -6,10 +6,33 @@ package simplefundtracker;
  */
 public class FundGrowthCalculator {
     private int months;
-    private final int percentGrowth;
+    private final int PERCENTGROWTH;
+    private Double yearGrowthRate;
+    private Double decadeGrowthRate;
     
 public FundGrowthCalculator(){
     this.months = 0;
-    this.percentGrowth = 0;
+    this.PERCENTGROWTH = 0;
 }
+public Double calculateYearGrowth(Double averageChange) {
+    
+    yearGrowthRate =  averageChange * 52.0;
+    return yearGrowthRate;
+    
+}
+
+public Double calculateDecadeGrowth(Double yearGrowthRate) {
+    
+    decadeGrowthRate = yearGrowthRate * 10.0;
+    return decadeGrowthRate;
+}
+
+public Double getYearGrowth() {
+    return yearGrowthRate;
+}
+
+public Double getDecadeGrowth() {
+    return decadeGrowthRate;
+}
+
 }
