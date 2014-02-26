340fdbf1efd9451486999a5e645bc6e0057ee2ee
Brandon Barrett <bbarrett@1105media.com>
2014-02-25 20:04:26 -0500

    Fix formatting errors in AverageCalculator class

--- SimpleFundTracker/src/simplefundtracker/AverageCalculator.java
+++ SimpleFundTracker/src/simplefundtracker/AverageCalculator.java
@@ -16,10 +16,10 @@ import java.text.DecimalFormat;
  */
 public class AverageCalculator {
     private BufferedReader mBr;
-    private String line;
-    private Double averageChange;
-    private Double d;
-    private String [] fundChangeArray;
+    private String         line;
+    private Double         averageChange;
+    private Double         d;
+    private String[]       fundChangeArray;
 
     public AverageCalculator() {
         averageChange = 0.0;
@@ -32,29 +32,25 @@ public class AverageCalculator {
 
         while ((line = mBr.readLine()) != null) {
             mBr.readLine();
-
-             fundChangeArray = line.split(" ");
+            fundChangeArray = line.split(" ");
 
             // Get average of fund change amount (stored in array at odd intervals)
             for (int i = 1; i < fundChangeArray.length; i += 2) {
-                d = Double.parseDouble(fundChangeArray[i]);
-
+                d             = Double.parseDouble(fundChangeArray[i]);
                 averageChange += d / i / 2;
             }
-            //Format average as a Double
+
+            // Format average as a Double
             DecimalFormat df = new DecimalFormat("#.##");
 
             System.out.println(df.format(averageChange));
         }
     }
-    
-  public Double getAverageChange() {
-        return averageChange;
-   }
- 
-}
-    
 
+    public Double getAverageChange() {
+        return averageChange;
+    }
+}
 
 
 
