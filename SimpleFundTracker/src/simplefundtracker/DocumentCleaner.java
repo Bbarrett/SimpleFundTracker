bc0b085ae7e2b87a7d56723d4fcb2fc34527413d
Brandon Barrett <bbarrett@1105media.com>
2014-02-26 20:52:03 -0500

    Begin creating cleanDuplicateDates() method

--- SimpleFundTracker/src/simplefundtracker/DocumentCleaner.java
+++ SimpleFundTracker/src/simplefundtracker/DocumentCleaner.java
@@ -1,38 +1,39 @@
-
-
 package simplefundtracker;
 
 //~--- JDK imports ------------------------------------------------------------
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
  * @author brandonbarrett
  */
-public class DocumentCleaner extends Recorder {
+public class DocumentCleaner extends Task {
+    private BufferedReader fileBr;
 
-    /**
-     *
-     * @throws java.io.IOException
-     */
-   
+    public DocumentCleaner() throws IOException {}
 
+    @Override
+    public void run() {}
 
-    public DocumentCleaner() throws IOException {
+    private void cleanDuplicateDates(File fundFile) throws IOException {
+        fileBr = new BufferedReader(new FileReader(fundFile));
 
+        String newLine   = fileBr.readLine();
+        String[] lineArray = newLine.split("");
+        Set tempSet   = new HashSet();
+
+        for (String str : lineArray) {
         
-        
-        
-    }
-
-    
-  }
             
         
-    
-
-
-
+        }
+    }
+}
 
