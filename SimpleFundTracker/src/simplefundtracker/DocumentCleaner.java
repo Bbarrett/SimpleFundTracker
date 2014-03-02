6d6ca7261c7aea9a5c92e1c8a9448d575a69a7f8
Brandon Barrett <Bbarrett@1105media.com>
2014-03-02 11:41:20 -0500

    Update DocumentCleaner.java

--- SimpleFundTracker/src/simplefundtracker/DocumentCleaner.java
+++ SimpleFundTracker/src/simplefundtracker/DocumentCleaner.java
@@ -1,7 +1,5 @@
 package simplefundtracker;
 
-//~--- JDK imports ------------------------------------------------------------
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
