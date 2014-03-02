09d5db279b4da4108956db396474d48bfa5526c0
Brandon Barrett <bbarrett@1105media.com>
2014-03-02 10:09:06 -0500

    Add elements to Fund class

--- SimpleFundTracker/src/simplefundtracker/Fund.java
+++ SimpleFundTracker/src/simplefundtracker/Fund.java
@@ -1,16 +1,31 @@
 package simplefundtracker;
 
+//~--- JDK imports ------------------------------------------------------------
+
+import java.io.IOException;
+
 /**
  *
  * @author brandonbarrett
  */
-public class Fund {
+public class Fund extends Connector {
     private String fundName;
     private int todaysValue;
+    private Double initialValue;
+    private Double yearlyContribution;
 
-    public Fund(String fundName) {
-        fundName = this.fundName;
+    public Fund(String fundName) throws IOException {
+        fundName     = this.getFundName();
+        initialValue = 150.00;
     }
+
+    public Double yearlyContributionTotal() {
+        yearlyContribution = initialValue * 26.0;
+
+        return yearlyContribution;
+    }
+    
+    
 }
 
 
