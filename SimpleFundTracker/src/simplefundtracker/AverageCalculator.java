43add418a23d5fd2b45afe46645096fbd54084af
Brandon Barrett <bbarrett@1105media.com>
2014-03-03 14:28:17 -0500

    Refactor into multiple methods (AverageCalculator)

--- SimpleFundTracker/src/simplefundtracker/AverageCalculator.java
+++ SimpleFundTracker/src/simplefundtracker/AverageCalculator.java
@@ -1,5 +1,7 @@
 package simplefundtracker;
 
+//~--- JDK imports ------------------------------------------------------------
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -13,32 +15,32 @@ import java.text.DecimalFormat;
  * @author brandonbarrett
  */
 public class AverageCalculator {
-    private BufferedReader mBr;
-    private String line;
     private Double averageChange;
-    private Double d;
+    private String line;
 
-    public AverageCalculator() {
-        averageChange = 0.0;
-    }
+    public AverageCalculator() {}
 
     public void readFile(File FundFile) throws FileNotFoundException, IOException {
-        mBr = new BufferedReader(new FileReader(FundFile));
+        BufferedReader mBr = new BufferedReader(new FileReader(FundFile));
 
         while ((line = mBr.readLine()) != null) {
             mBr.readLine();
+        }
+    }
 
-            String[] fundChangeArray = line.split(" ");
+    public Double calculateAverageChange() {
+        String[] fundChangeArray = line.split(" ");
 
-            for (int i = 1; i < fundChangeArray.length; i += 2) {
-                d = Double.parseDouble(fundChangeArray[i]);
-                averageChange += d / i / 2;
-            }
+        for (int i = 1; i < fundChangeArray.length; i += 2) {
+            Double d = Double.parseDouble(fundChangeArray[i]);
+
+            averageChange += d / i / 2;
 
             DecimalFormat df = new DecimalFormat("#.##");
-
-            System.out.println(df.format(averageChange));
+            df.format(d);
         }
+
+        return averageChange;
     }
 
     public Double getAverageChange() {
