9b22cb2bb41e204c87915e01c0e590f22838d6d5
Brandon Barrett <bbarrett@1105media.com>
2014-03-03 14:29:54 -0500

    pass parameters into yearlyConributionTotal method

--- SimpleFundTracker/src/simplefundtracker/Fund.java
+++ SimpleFundTracker/src/simplefundtracker/Fund.java
@@ -6,17 +6,16 @@ import java.io.IOException;
  * @author brandonbarrett
  */
 public class Fund extends Connector {
-    private String fundName;
-    private int todaysValue;
+ 
     private Double initialValue;
     private Double yearlyContribution;
 
     public Fund(String fundName) throws IOException {
-        fundName     = this.getFundName();
+        
         initialValue = 150.00;
     }
 
-    public Double yearlyContributionTotal() {
+    public Double yearlyContributionTotal(Double initialValue) {
         yearlyContribution = initialValue * 26.0;
 
         return yearlyContribution;
