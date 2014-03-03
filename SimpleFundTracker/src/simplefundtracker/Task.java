afada0ec4bbd137f794c631ae8d76875d8194e68
Brandon Barrett <bbarrett@1105media.com>
2014-03-03 14:19:57 -0500

    Update class, reduce coupling

--- SimpleFundTracker/src/simplefundtracker/Task.java
+++ SimpleFundTracker/src/simplefundtracker/Task.java
@@ -2,17 +2,13 @@ package simplefundtracker;
 
 import java.io.IOException;
 import java.util.TimerTask;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author brandonbarrett
  */
 public class Task extends TimerTask {
-    private Recorder r;
-    private Connector c;
-    private String todaysFund;
+   
     private final URLScanner URLSC;
 
     public Task() throws IOException {
@@ -23,18 +19,8 @@ public class Task extends TimerTask {
     // Make it simple, create a task that can be set to run each day at noon.
     @Override
     public void run() {
-        try {
-            r = new Recorder();
-            c = new Connector();
-
-            String fundURL = URLSC.getURL();
-
-            c.findFund(fundURL);
-            r.createHeader();
-            todaysFund = c.getfundElement();
-            r.createFund(todaysFund);
-        } catch (IOException ex) {
-            Logger.getLogger(Task.class.getName()).log(Level.SEVERE, null, ex);
+       
+          
         
     
         }
@@ -42,4 +28,4 @@ public class Task extends TimerTask {
 
 
 
-}
+
