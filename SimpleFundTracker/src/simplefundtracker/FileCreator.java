b08334a35e79297acf1b98c8647a2b02071ece9d
Brandon Barrett <bbarrett@1105media.com>
2014-03-03 11:36:46 -0500

    Update class, reduce coupling

---
+++ SimpleFundTracker/src/simplefundtracker/FileCreator.java
@@ -0,0 +1,29 @@
+package simplefundtracker;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ *
+ * @author brandonbarrett
+ */
+public class FileCreator {
+    private final File FUND_FILE;
+
+    public FileCreator() throws IOException {
+        FUND_FILE = new File("/Users/brandonbarrett/fundFile.txt");
+
+        if (!FUND_FILE.exists()) {
+            FUND_FILE.createNewFile();
+        } else {
+            System.out.println("File already exists.");
+        }
+    }
+
+    public File getFundFile() {
+        return FUND_FILE;
+    }
+}
+
+
+
