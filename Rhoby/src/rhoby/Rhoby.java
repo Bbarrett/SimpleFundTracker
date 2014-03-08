ce69573991a3f8e9d56e2e809d3cdd37576e2e50
Brandon Barrett <bbarrett@1105media.com>
2014-03-08 15:18:14 -0500

    Commit class

---
+++ Rhoby/src/rhoby/Rhoby.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rhoby;
+
+/**
+ *
+ * @author brandonbarrett
+ */
+public class Rhoby {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
