4df642f30b10f2c5003757e7ba36b75f044a23d5
Brandon Barrett <bbarrett@1105media.com>
2014-03-08 13:56:00 -0500

    Update class

--- SimpleFundTracker/src/simplefundtracker/DocumentCleaner.java
+++ SimpleFundTracker/src/simplefundtracker/DocumentCleaner.java
@@ -6,8 +6,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
@@ -18,25 +16,12 @@ public class DocumentCleaner extends Task {
 
     public DocumentCleaner() throws IOException {}
 
-    @Override
-    public void run() {
-        try {
-            Recorder r = new Recorder();
-            File fundFile = r.getFundFile();
-            String Date = r.getDate().toString();
-
-            cleanDuplicateDates(fundFile, Date);
-        } catch (IOException ex) {
-            Logger.getLogger(DocumentCleaner.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
     private void cleanDuplicateDates(File fundFile, String Date) throws IOException {
         fileBr = new BufferedReader(new FileReader(fundFile));
 
-        String   newLine = fileBr.readLine();
+        String   newLine   = fileBr.readLine();
         String[] lineArray = newLine.split("");
-        Set testSet = new HashSet();
+        Set testSet   = new HashSet();
 
         for (String str : lineArray) {
             if (!testSet.add(Date)) {
@@ -45,5 +30,6 @@ public class DocumentCleaner extends Task {
         }
     }
 }
+
 
 
